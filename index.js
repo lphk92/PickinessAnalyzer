046e0c9dd02962ff8e5245389a61bfd10203ad8a
Lucas Kushner <lucas@crunchbang.(none)>
2014-03-26 13:48:28 -0400

    Began adding the processing script for the data

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var submit = document.getElementById("submit");
+submit.onclick = processData;
 
 function processData()
 {
-
+    var ranges = [];
+    
+    var inputs = document.getElementsByTagName("input");
+    alert("Process");
+    for (var i = 0 ; i < inputs.length ; i++)
+    {
+        var element = inputs[i];
+        if (element.type == "range")
+            ranges.push(element);
+    }
 }
